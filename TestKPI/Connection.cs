d143e7b3d2c07612a69c10eeb1622627ba17c881
Mikhailo Kholmohorov <mykhailo1257@gmail.com>
2020-08-28 16:58:35 +0300

    Add Database Connection

---
+++ TestKPI/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TestKPI
+{
+    class Connection
+    {
+        private static SqlConnection instance;
+        public string connectionString { get; private set; }
+        protected Connection(string constr = @"Data Source=LAPTOP-E1KKJ6T9\SQLEXPRESS;Initial Catalog=Conferences;Integrated Security=True")
+        {
+            this.connectionString = constr;
+        }
+        public static SqlConnection getInstance(string constr = @"Data Source=LAPTOP-E1KKJ6T9\SQLEXPRESS;Initial Catalog=Conferences;Integrated Security=True")
+        {
+            if (instance == null)
+                instance = new SqlConnection(constr);
+            return instance;
+        }
+    }
+
+    class ConnectionToDB
+    {
+        public SqlConnection ConnectionDB { get; private set; }
+        public void SetConnection()
+        {
+            ConnectionDB = Connection.getInstance();
+        }
+    }
+}
--- TestKPI/Program.cs
+++ TestKPI/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace TestKPI
 {
@@ -10,6 +12,22 @@ namespace TestKPI
     {
         static void Main(string[] args)
         {
+            ConnectionToDB connect = new ConnectionToDB();
+            connect.SetConnection();
+            try
+            {
+                connect.ConnectionDB.Open();
+                Console.WriteLine("Connection open");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connect.ConnectionDB.Close();
+                Console.WriteLine("Connection close");
+            }
         }
     }
 }
